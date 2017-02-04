0da93a62d6c4c6961dee3dc395d0f9b5de3a6ba0
404random <hershgupta98@gmail.com>
2017-02-04 17:36:30 -0500

    Add files via upload

---
+++ MainPage.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Threading.Tasks;
+using Windows.ApplicationModel.Calls;
+using unirest_net.http;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.System;
+using Windows.UI.Core;
+using Windows.UI.ViewManagement;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using Newtonsoft.Json;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace BrownHackathon
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private void OnEnter(object sender, KeyRoutedEventArgs e)
+        {
+            if (e.Key == VirtualKey.Enter)
+            {
+                String input = Input.Text;
+                Debug.Write(input);
+                Input.Text = "";
+               HttpResponse response = Unirest.post("https://textanalysis-text-summarization.p.mashape.com/text-summarizer-text")
+                    .header("X-Mashape-Key", "SUf4NVLPMxmshlTgrjCpu0Lfawynp1vyziEjsnWvrXoNP1g7AS")
+                    .header("Content-Type", "application/x-www-form-urlencoded")
+                    .header("Accept", "application/json")
+                    .field("sentnum", 5)
+                    .field("text", "Automatic summarization is the process of reducing a text document with a computer program in order to create a summary that retains the most important points of the original document. As the problem of information overload has grown, and as the quantity of data has increased, so has interest in automatic summarization. Technologies that can make a coherent summary take into account variables such as length, writing style and syntax. An example of the use of summarization technology is search engines such as Google. Document summarization is another.")
+                    .asJson<MainPage>();
+                String answer = JsonConvert.DeserializeObject<String>(response.ToString());
+                Debug.WriteLine(answer);
+
+            }
+        }
+    }
+}
